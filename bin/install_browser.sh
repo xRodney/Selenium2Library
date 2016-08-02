002619c7e41aca45886802a5fb4249518dc6c810
Dmitriy Robota <dmitriy.robota@gmail.com>
2016-08-02 15:14:33 -0700

    Update install browser

--- bin/install_browser.sh
+++ bin/install_browser.sh
@@ -4,8 +4,8 @@ if [[ "${BROWSER}" == "chrome" ]]
 then
     # installing Chrome
     sudo apt-get install -q -y libxss1 libappindicator1 libindicator7
-    wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i google-chrome*.deb
+    wget -q -O ~/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i ~/google-chrome*.deb
     sudo apt-get install -f -y
     google-chrome --version
     # installing chromedriver
@@ -22,6 +22,7 @@ elif [[ "${BROWSER}" == "phantomjs" ]]
 then
     wget -O ~/phantomjs-2.1.1-linux-x86_64.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
     tar xjf ~/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C ~
+    rm -f /usr/local/bin/phantomjs
     sudo ln -s ~/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
 else
     # installing Firefox to get all dependencies
